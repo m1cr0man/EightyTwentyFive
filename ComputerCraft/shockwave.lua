850e23bac538c3f310883e4e7343b5f2ee0fcc5a
Lucas Savva <lucas@vccomputers.ie>
2016-12-11 01:45:09 +0000

    Major improvements to shockwave - Add Ender chest support - Improve efficiency of restack function - Use better algorithm for torch placement - Fix makeEmptySlot tries to restack full stacks

--- ComputerCraft/shockwave.lua
+++ ComputerCraft/shockwave.lua
@@ -6,13 +6,27 @@ local blacklist = {
 	"cobblestone",
 	"dirt"
 }
-local torch_gap = 15
 
+-- Min and max gaps between torch placement
+local min_torch_gap = 10
+local max_torch_gap = 14
+
+-- Slot to store torches
+local torch_slot = 16
+
+-- Slot to store ender chest
+local chest_slot = 15
+
+-- Last slot to use for items
+local item_slot_max = 14
+
+-- Sort out arguments
 local num_rings = tonumber(argv[1]) or 5
 local center_width = tonumber(argv[2]) or 4
 local gap = tonumber(argv[3]) or 2
 local start_ring = tonumber(argv[4]) or 1
-local torch_distance = 0
+
+-- Keep a record of what is found
 local findings = {}
 if (fs.exists("findings.txt")) then
 	local file = io.open("findings.txt", "r")
@@ -20,19 +34,13 @@ if (fs.exists("findings.txt")) then
 	file:close()
 end
 
-local function refuel(quant)
-	local old_slot = turtle.getSelectedSlot()
-	for slot = 1, 15 do
-		turtle.select(slot)
-		if turtle.refuel(quant) then break end
-	end
-	turtle.select(old_slot)
-end
-
+-- Restacks items in the inventory
 local function restack()
 	local old_slot = turtle.getSelectedSlot()
 	local inv_map = {}
-	for slot = 1, 15 do
+
+	-- Restack all slots
+	for slot = 1, item_slot_max do
 		local detail = turtle.getItemDetail(slot) or {}
 		if detail.name then
 			local full_name = detail.name .. '#' .. detail.damage or 0
@@ -45,7 +53,7 @@ local function restack()
 				if turtle.getItemSpace(inv_map[full_name]) == 0 then
 					inv_map[full_name] = slot
 				end
-			else
+			elseif turtle.getItemSpace(slot) > 0 then
 				inv_map[full_name] = slot
 			end
 		end
@@ -53,23 +61,11 @@ local function restack()
 	turtle.select(old_slot)
 end
 
-local function getEmptySlot()
-
-	-- Attempt to free a slot just by moving things around
-	restack()
-
-	for slot = 1, 15 do
-		if turtle.getItemCount(slot) == 0 then
-			return slot
-		end
-	end
-	return 0
-end
-
+-- Drops item on the blacklist
 local function dropBlacklist()
 	local dropped_stuff = false
 	local old_slot = turtle.getSelectedSlot()
-	for slot = 1, 15 do
+	for slot = 1, item_slot_max do
 		local detail = turtle.getItemDetail(slot) or {}
 		for _, word in pairs(blacklist) do
 			if detail.name:find(word) then
@@ -82,19 +78,95 @@ local function dropBlacklist()
 		end
 	end
 	turtle.select(old_slot)
-	return dropped_stuff
 end
 
-local function placeTorch()
-	if torch_gap ~= torch_distance then
-		torch_distance = torch_distance + 1
-		return
-	end
-	turtle.select(16)
+-- Deposit ores, refuel, and restock torches from ender chest
+local function enderChestDeposit()
+	local old_slot = turtle.getSelectedSlot()
+	local has_refuelled = false
+
+	-- Place the chest
+	print("Depositing items")
+	turtle.digDown()
+	turtle.select(chest_slot)
 	turtle.placeDown()
-	turtle.select(1)
-	torch_distance = 0
+
+	-- Grab some torches
+	-- Should be in first slot of the chest
+	local num_torches = turtle.getItemSpace(torch_slot)
+	turtle.select(torch_slot)
+	turtle.suckDown(num_torches)
+
+	-- Deposit resources
+	for slot = 1, item_slot_max do
+		turtle.select(slot)
+
+		-- Grab some fuel from what's in our inventory
+		-- before depositing it
+		if not has_refuelled then
+			has_refuelled = turtle.refuel(5)
+		end
+
+		turtle.dropDown()
+	end
+
+	-- Pick up the chest again
+	turtle.select(chest_slot)
+	turtle.digDown()
+	turtle.select(old_slot)
+	print("Done")
+end
+
+-- Gets the slot number of a free slot
+-- Returns 0 if there are no free slots
+local function getEmptySlot()
+	for slot = 1, item_slot_max do
+		if turtle.getItemCount(slot) == 0 then
+			return slot
+		end
+	end
+	return 0
+end
+
+local function makeEmptySlot()
+	local options = {
+		restack,
+		dropBlacklist,
+		enderChestDeposit
+	}
+
+	-- Try all the above options to free a slot
+	-- enderChestDeopsit will always succeed
+	local slot = getEmptySlot();
+	for _, func in ipairs(options) do
+		if slot ~= 0 then return slot end
+		func()
+		slot = getEmptySlot()
+	end
+
+	return true
+end
+
+local function placeTorchInternal()
+	if turtle.detectDown() then return end
+	local old_slot = turtle.getSelectedSlot()
+	turtle.select(torch_slot)
+	turtle.placeDown()
+	turtle.select(old_slot)
 	print("Placed torch")
+end
+
+local function placeTorch(column, distance)
+
+	-- Don't place them too far apart
+	local gap;
+	for count = 2, distance do
+		gap = distance / count
+		if gap < max_torch_gap then break end
+	end
+
+	-- Place a torch at the right intervals
+	if column == 1 or column % math.ceil(gap) == 0 then placeTorchInternal() end
 end
 
 local function checkInventory()
@@ -110,7 +182,7 @@ local function checkInventory()
 
 	-- Check if inventory is full
 	-- Try to free space
-	if getEmptySlot() == 0 and not dropBlacklist() then
+	if not makeEmptySlot() then
 		print("Clear inventory space and press enter")
 		read()
 	end
@@ -129,7 +201,7 @@ local function inspectBlocks()
 	file:close()
 end
 
-local function digDistance(distance)
+local function digDistance(distance, skip_torches)
 	for column = 1, distance do
 
 		-- Record what we're digging
@@ -139,10 +211,13 @@ local function digDistance(distance)
 		checkInventory()
 
 		repeat until not turtle.digUp()
-		turtle.digDown()
+
+		-- Check item below isn't a torch and dig it
+		local success, detail = turtle.inspectDown()
+		if success and not detail.name:match("torch") then turtle.digDown() end
 
 		-- Place a torch if necessary
-		placeTorch()
+		if not skip_torches then placeTorch(column, distance) end
 
 		repeat turtle.dig() until turtle.forward()
 	end
@@ -164,9 +239,9 @@ local function shockwave(num_rings, center_width)
 
 		-- Move to next ring
 		if ring ~= num_rings then
-			digDistance(gap + 1)
+			digDistance(gap + 1, true)
 			turtle.turnLeft()
-			digDistance(gap + 1)
+			digDistance(gap + 1, true)
 			for _ = 1, 2 do turtle.turnLeft() end
 		end
 	end
@@ -179,7 +254,8 @@ local function main()
 			return
 		end
 	end
-	print("Make sure torches are in slot 16 and press enter")
+	print(("Torches slot: %d\nEnder Chest slot: %d"):format(torch_slot, chest_slot))
+	print("Press enter when ready")
 	read()
 	print("Starting")
 	shockwave(num_rings, center_width)
