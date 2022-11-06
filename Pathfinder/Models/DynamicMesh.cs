d12ed4f14d60654187b9b907705c397e28fa2ac8
Lucas Savva <lucas@m1cr0man.com>
2022-11-06 01:22:56 +0000

    Add reminder

---
+++ Pathfinder/Models/DynamicMesh.cs
@@ -0,0 +1,3 @@
+# From what I remember, the plan here was to generate a virtual mesh of
+# block probabilities, filled in based on the real data.
+# I think I already did this over in the ts->lua OC stuff
