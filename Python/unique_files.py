42ca6ef7c11bcd156142046b6334ed21b1a7d8fb
Lucas Savva <lucas@m1cr0man.com>
2022-11-06 01:58:32 +0000

    unique_files.py: More features

--- Python/unique_files.py
+++ Python/unique_files.py
@@ -1,13 +1,15 @@
 from pathlib import Path
+import shutil
 import sys
 from argparse import ArgumentParser
 from multiprocessing import Pool
+from typing import Optional
 from rich import print
 from rich_argparse import RichHelpFormatter
 
 
-def read_dir(path: Path) -> set[str]:
-    return {file.name for file in path.rglob("*") if not file.is_dir()}
+def read_dir(path: Path) -> set[Path]:
+    return {file for file in path.rglob("*") if not file.is_dir()}
 
 
 def main(args: list[str]) -> int:
@@ -29,6 +31,18 @@ def main(args: list[str]) -> int:
         default=False,
         action="store_true",
     )
+    parser.add_argument(
+        "--copy-missing-to",
+        help="Where to copy unique files to",
+        type=Path,
+        default=None,
+    )
+    parser.add_argument(
+        "--delete-missing",
+        help="Delete missing/mismatched files (happens after copy if specified)",
+        default=False,
+        action="store_true",
+    )
 
     parsed_args = parser.parse_args(args)
 
@@ -43,15 +57,36 @@ def main(args: list[str]) -> int:
     print("Loaded", len(files_a), "files from", dir_a)
     print("Loaded", len(files_b), "files from", dir_b)
 
+    dict_a = {f.name: f for f in files_a}
+    dict_b = {f.name: f for f in files_b}
+    set_a = set(dict_a.keys())
+    set_b = set(dict_b.keys())
+
+    dest: Optional[Path] = parsed_args.copy_missing_to
+
+    if dest and not dest.exists():
+        print(f"Creating '{dest}'")
+        dest.mkdir()
+
     if not parsed_args.b_only:
-        print(f"Unique files in {dir_a}")
-        for name in files_a.difference(files_b):
+        print(f"Unique files in '{dir_a}'")
+        for name in set_a.difference(set_b):
             print(name)
+            if dest:
+                shutil.copy2(str(dict_a[name]), dest)
+            if parsed_args.delete_missing:
+                print(f"Deleting '{dict_a[name]}'")
+                dict_a[name].unlink()
 
     if not parsed_args.a_only:
-        print(f"Unique files in {dir_b}")
-        for name in files_b.difference(files_a):
+        print(f"Unique files in '{dir_b}'")
+        for name in set_b.difference(set_a):
             print(name)
+            if dest:
+                shutil.copy2(str(dict_b[name]), dest)
+            if parsed_args.delete_missing:
+                print(f"Deleting '{dict_b[name]}'")
+                dict_b[name].unlink()
 
     return 0
 
